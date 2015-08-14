9bba94b0c44bf26c740f74a2a4ad533a657dabd9
Stefan <stefan.hoerterer@gmail.com>
2015-08-14 17:30:03 +0200

    added file testing

--- src/userinitials/UserInitials.php
+++ src/userinitials/UserInitials.php
@@ -8,22 +8,54 @@
 
 namespace userinitials;
 
-
+/**
+ * Class UserInitials
+ * @package userinitials
+ */
 class UserInitials {
 
   private $options;
 
-  public function __construct($options) {
+  /**
+   * Constructor with optional $options
+   * @param array $options
+   *   An arary with options
+   * @see UserInitials::getDefaultOptions() for available options
+   */
+  public function __construct($options = array()) {
 
     $default_options = UserInitials::getDefaultOptions();
     $this->options = array_merge($default_options, $options);
 
   }
 
+  /**
+   * Get an option
+   * @param $name
+   *   The name/key of the option
+   * @return mixed
+   * @see UserInitials::getDefaultOptions() for available options
+   */
   public function getOption($name) {
     return $this->options[$name];
   }
 
+  /**
+   * Set an option
+   * @param $name
+   *   The name/key of the option
+   * @param $val
+   *   The value of the option
+   * @see UserInitials::getDefaultOptions() for available options
+   */
+  public function setOption($name, $val) {
+    $this->options[$name] = $val;
+  }
+
+  /**
+   * A list of default options
+   * @return array
+   */
   public static function getDefaultOptions() {
     $options = array();
     $options['backgroundcolor'] = '#d6dadc';
@@ -35,17 +67,31 @@ class UserInitials {
 
   }
 
+  /**
+   * Create the image as an svg file
+   * @param $text
+   *   The initials string
+   * @param $filename
+   *   The filename, relative or absolute path)
+   */
   public function createSVGFile($text, $filename) {
     $svg = $this->createSVG($text);
     file_put_contents($filename, $svg);
   }
 
+  /**
+   * Generate svg xml for the text $text
+   * @param $text
+   *  The initials string
+   * @return string
+   *   The svg xml
+   */
   public function createSVG($text) {
 
     return <<<EOF
       <svg width="{$this->getOption('width')}" height="{$this->getOption('height')}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
        <g>
-        <rect id="svg_2" height="80" width="80" rx="4" ry="4" stroke-linecap="null" stroke-linejoin="null" stroke-width="0" stroke="#000000" fill="{$this->getOption('backgroundcolor')}"/>
+        <rect id="svg_2" height="80" width="80" rx="{$this->getOption('width')}" ry="{$this->getOption('width')}" stroke-linecap="null" stroke-linejoin="null" stroke-width="0" stroke="#000000" fill="{$this->getOption('backgroundcolor')}"/>
         <text x="40" y="44" font-size="38" font-family="verdana,sans-serif" text-anchor="middle" alignment-baseline="middle" stroke="#000000" fill="{$this->getOption('color')}">$text</text>
        </g>
       </svg>
--- tests/UserInitialsTest.php
+++ tests/UserInitialsTest.php
@@ -6,8 +6,23 @@
  * Date: 14.08.2015
  * Time: 14:14
  */
-class UserInitialsTest extends PHPUnit_Framework_TestCase {
 
+namespace userinitials\tests;
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
+
+class UserInitialsTest extends \PHPUnit_Framework_TestCase {
+  private $root;
+
+  public function setUp()
+  {
+    $this->root = vfsStream::setup("test");
+  }
+
+  /**
+   * test options override
+   */
   public function testOptions() {
 
     $options = array(
@@ -19,4 +34,23 @@ class UserInitialsTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals($userinitials->getOption("width"), 2000);
 
   }
+
+  /**
+   * Test fiel creation
+   */
+  public function testSVGCreationWorks(){
+    $userinitials = new \userinitials\UserInitials();
+
+
+    $testStreamUrl = vfsStream::url('test/test.svg');
+
+    $userinitials->createSVGFile("SH",$testStreamUrl);
+
+
+    $this->assertTrue($this->root->hasChild("test.svg"));
+
+    $this->assertTrue($this->root->getChild("test.svg")->size() > 0);
+
+  }
+
 }
